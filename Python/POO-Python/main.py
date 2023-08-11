80a0881a8033f0175e1f84f3d22920154ed66903
silascunha97 <119726629+silascunha97@users.noreply.github.com>
2023-08-11 14:54:17 -0300

    Add files via upload

---
+++ Python/POO-Python/main.py
@@ -0,0 +1,15 @@
+from pessoa import Pessoa
+""""
+p1 = Pessoa()
+p2 = Pessoa()
+
+print(p1)
+print(p2)
+
+p1.nome= 'Silas'
+p2.nome= 'João'
+
+print(p1.nome)
+print(p2.nome)
+""""
+
---
+++ Python/POO-Python/poo001.py
@@ -0,0 +1,9 @@
+def fun(x, y):
+    return x*y
+
+class Pessoa:
+    
+    def falar(self):
+        print('Pessoa está falando...')
+    
+    pass
