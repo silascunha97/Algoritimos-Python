be0baf00b3b521f4e20a748f2938f0a160a944f2
silascunha97 <119726629+silascunha97@users.noreply.github.com>
2023-08-11 08:19:02 -0300

    Add files via upload

---
+++ Python/aula021.py
@@ -0,0 +1,9 @@
+tab = 1
+while tab <= 10:
+    num = 1
+    print(f"*** tabuada do {tab} *** ")
+    while num <= 10:
+        print(f"{tab} x {num} = {tab*num}")
+        num += 1
+    tab += 1
+print()
